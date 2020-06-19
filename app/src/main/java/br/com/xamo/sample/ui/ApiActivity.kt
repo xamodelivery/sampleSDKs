a00eb449453f5fe632128df1a6fed09041345ea5
Andre Freitas <anascimento.developer@gmail.com>
2020-06-19 17:33:34 -0300

    atualizacao ApiActivity

--- app/src/main/java/br/com/xamo/sample/ui/ApiActivity.kt
+++ app/src/main/java/br/com/xamo/sample/ui/ApiActivity.kt
@@ -6,7 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import br.com.xamo.sample.AppApplication
 import br.com.xamo.sample.R
 import br.com.xamo.sdk.api.endpoints.loja.XamoApiLoja
-import kotlinx.android.synthetic.main.teste_layout.*
+import br.com.xamo.sdk.api.model.serialization.cadastroLoja.CadastroLoja
+import kotlinx.android.synthetic.main.api_layout.*
 import kotlinx.coroutines.*
 
 class ApiActivity : AppCompatActivity() {
@@ -15,23 +16,77 @@ class ApiActivity : AppCompatActivity() {
         (application as AppApplication).getXamoApiSdk().loja()
     }
 
+    companion object {
+        const val TAG = "ApiActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.teste_layout)
+        setContentView(R.layout.api_layout)
 
-        button.setOnClickListener {
-            apiGetLoginLoja()
+        btnDadosLoja.setOnClickListener {
+            dadosLoja()
+        }
+
+        btnCadastroLoja.setOnClickListener {
+            cadastroLoja()
         }
     }
 
-    private val ceh = CoroutineExceptionHandler { _, throwable ->
-        Log.e("XAMOAPI","CEH Handled Crash [$throwable]")
+    private val viewModelJob = SupervisorJob()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModelJob.cancel()
     }
 
-    fun apiGetLoginLoja() {
-        GlobalScope.launch(Dispatchers.Main + ceh) {
+    fun dadosLoja() {
+        uiScope.launch(CoroutineExceptionHandler { _, exception ->
+            Log.e(TAG, exception.message, exception)
+        }) {
             val result = apiLoja.getLoja().await()
-            Log.d("XAMOAPI", result.id ?: "")
+            Log.d(TAG, result.toString())
+        }
+    }
+
+    fun cadastroLoja() {
+        uiScope.launch(CoroutineExceptionHandler { _, exception ->
+            Log.e(TAG, exception.message, exception)
+        }) {
+
+            val categorias = apiLoja.getCategorias().await()
+            val categoria = categorias[0]
+
+            val cadastro = CadastroLoja()
+                .endereco(
+                    CadastroLoja.Endereco()
+                        .cidade("Manaus")
+                        .estado("Amazonas")
+                        .logradouro("Rua Saturno")
+                        .numero("458")
+                        .bairro("Jardim das acacias")
+                        .cep("32241-340")
+                        .complemento(null)
+                        .latitude(-23.5707855)
+                        .longitude(-46.6434994)
+                )
+                .loja(
+                    CadastroLoja.Loja()
+                        .telefone("+5592981470275")
+                        .cnpj(null)
+                        .cpf("99170278364")
+                        .rg(null)
+                        .nomeProprietario("André Nascimento")
+                        .email(null)
+                        .nomeLoja("Mercadinho do Zé")
+                        .categoria(categoria)
+                        .fotoComprovanteEndereco("")
+                        .fotoRgCnhProprietario("")
+                )
+
+            val result = apiLoja.cadastro(cadastro).await()
+            Log.d(TAG, result.toString())
         }
     }
 }
--- app/src/main/java/br/com/xamo/sample/ui/SplashActivity.kt
+++ app/src/main/java/br/com/xamo/sample/ui/SplashActivity.kt
@@ -2,56 +2,95 @@ package br.com.xamo.sample.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import br.com.xamo.sample.AppApplication
 import br.com.xamo.sample.R
+import br.com.xamo.sdk.api.endpoints.loja.XamoApiLoja
+import br.com.xamo.sdk.api.model.dto.loja.LojaDTO
+import br.com.xamo.sdk.api.model.dto.loja.StatusLojaEnum
 import br.com.xamo.sdk.login.ui.LoginNavegacao
 import br.com.xamo.sdk.login.ui.LoginTipoApp
 import br.com.xamo.sdk.login.utils.LoginUtils
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SPLASH"
+        const val CODE = 0
+    }
+
+    private val apiLoja : XamoApiLoja by lazy {
+        (application as AppApplication).getXamoApiSdk().loja()
+    }
+
+    private val viewModelJob = SupervisorJob()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
+        //LoginUtils.logout()
         checarLogin()
     }
 
-    companion object {
-        const val CODE = 0
-    }
-
-    fun checarLogin() {
-        GlobalScope.launch(Dispatchers.Main) {
-            //LoginUtils.logout()
-            delay(2000)
-            if (LoginUtils.usuarioLogado()) {
-                navelarTelaUsuarioLogado()
-            } else {
-                LoginNavegacao.navegarTelaEscolhaAutenticacao(this@SplashActivity, LoginTipoApp.LOJA, CODE)
-            }
-        }
-    }
-
-    fun navelarTelaUsuarioLogado() {
-        val intent = Intent(this@SplashActivity, ApiActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        startActivity(intent)
-        ActivityCompat.finishAffinity(this@SplashActivity)
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModelJob.cancel()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == CODE) {
             if (resultCode == RESULT_OK && data != null) {
-                //val model : LojaDTO = LoginNavegacao.getResultLoginLoja(data)
-                navelarTelaUsuarioLogado()
+                val loja : LojaDTO = LoginNavegacao.getResultLoginLoja(data)
+                verificaTelaSeguinte(loja)
             }
         }
+    }
+
+    fun checarLogin() {
+        uiScope.launch(CoroutineExceptionHandler { _, exception ->
+            Log.e(TAG, exception.message, exception)
+        }) {
+            delay(2000)
+            if (LoginUtils.usuarioLogado()) {
+                val loja : LojaDTO = apiLoja.getLoja().await()
+                verificaTelaSeguinte(loja)
+            } else {
+                LoginNavegacao.navegarTelaEscolhaAutenticacao(this@SplashActivity, LoginTipoApp.LOJA, CODE)
+            }
+        }
+    }
+
+    fun verificaTelaSeguinte(loja : LojaDTO) {
+        when {
+            loja.status!! == StatusLojaEnum.NAO_FINALIZADO -> {
+                Log.i(TAG, "ir para tela de cadastro")
+            }
+            loja.status!! == StatusLojaEnum.APROVADO -> {
+                Log.i(TAG, "ir para tela de aprovado")
+            }
+            loja.status!! == StatusLojaEnum.RECUSADO -> {
+                Log.i(TAG, "ir para tela de recusado")
+            }
+            loja.status!! == StatusLojaEnum.EM_ANALISE -> {
+                Log.i(TAG, "ir para tela de analise")
+            }
+            loja.status!! == StatusLojaEnum.ATIVO -> {
+                Log.i(TAG, "ir para tela de home principal usuario logado")
+            }
+        }
+        navelarTelApis()
+    }
+
+    fun navelarTelApis() {
+        val intent = Intent(this@SplashActivity, ApiActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        ActivityCompat.finishAffinity(this@SplashActivity)
     }
 }
